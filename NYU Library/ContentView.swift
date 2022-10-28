0dc7856d1894059f8ae14c767dc86e9f23d35a61
Brayton-Lordianto <braylord30@gmail.com>
2022-10-28 19:07:05 -0400

    first day

--- NYU Library/ContentView.swift
+++ NYU Library/ContentView.swift
@@ -8,13 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            
+        
+        TabView {
+            ListView()
+                .tabItem {
+                    icon("house", "Home")
+                }
+
+            Text("")
+                .tabItem {
+                    icon("safari", "Tour")
+                }
+
+            Text("")
+                .tabItem {
+                    icon("calendar", "Calendar")
+                }
         }
-        .padding()
     }
 }
+
+extension ContentView {
+    func icon(_ name: String, _ label: String) -> some View {
+        VStack {
+            Image(systemName: name)
+            Text(label)
+        }
+    }
+    
+    
+}
+
+//struct CalendarView
+
+struct HomeView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct ListView: View {
+    var body: some View {
+        VStack {
+            List {
+                VStack {
+                    HStack {
+                        Image(systemName: "person")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                        VStack(alignment: .leading) {
+                            Text("Library Tour")
+                                .bold()
+                            Text("Description of Image")
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ NYU Library/ReservationViews/SelectCalendar.swift
@@ -0,0 +1,20 @@
+//
+//  SelectCalendar.swift
+//  NYU Library
+//
+//  Created by Brayton Lordianto on 10/28/22.
+//
+
+import SwiftUI
+
+struct SelectCalendar: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct SelectCalendar_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectCalendar()
+    }
+}
