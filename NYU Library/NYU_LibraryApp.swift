13eb39043206bab501d64b0cf8ea88ddf5c5c271
Stacey Lee <lisa888868@icloud.com>
2022-10-28 17:40:48 -0400

    Initial Commit

---
+++ NYU Library/NYU_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYU_LibraryApp.swift
+//  NYU Library
+//
+//  Created by student on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct NYU_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
